4cfb5f130243d06337d50754b56cc19ec2b6d3a8
juliarias20 <arias.julie05@gmail.com>
2025-04-30 14:05:39 -0700

    Added base64_encrypt() function

--- ciphers.c
+++ ciphers.c
@@ -19,6 +19,13 @@
 void caesarCipherEncryption();
 void caesarCipherDecryption();
 
+void base64_Encrypt();
+char convertToBase();
+char* convertToBinary();
+char* split_array();
+int convertToDecimal();
+
+
 int main() {
 	char input;
 
@@ -47,7 +54,7 @@ int main() {
 			printf("Invalid selection.\n");
 		}
 	}
-	/*
+	
 	if (input == 'b') {
 		printf("Selected Cipher: Base64\n");
 		printf("Mode:\n");
@@ -57,16 +64,16 @@ int main() {
 		scanf(" %c", &input);
 
 		if (input == 'e') {
-			run base64 encryption function
+			base64_Encrypt();
 		}
 		if (input == 'd') {
-			run base64 decryption function
+			// run decryp function
 		}
 		else {
-			printf("Invalid selection.\n");
+			//printf("Invalid selection.\n");
 		}
 	}
-	if (input == 'a') {
+	/*if (input == 'a') {
 		printf("Selected Cipher: Atbash Cipher\n");
 		printf("Mode:\n");
 		printf("\te: Encrypt\n");
@@ -155,7 +162,125 @@ void caesarCipherDecryption() {
 }
 
 //Base64 Function --- will be added later
-void base64_Encrypt(){
+void base64_Encrypt()
+{
+	char user_string[100];
+    int ascii_array[100];
+    char binaryString [1000] = "";
+    char spaced_array [2000] = "";
+
+    char base64_string [100]; 
+
+    int array_count = 0;
+
+    // Ask for string for encryption
+    printf("Selected mode: Encryption\nPlease put string to encrypt: ");
+    scanf("%s", user_string);
+
+    // STEP 1: Convert each char in the array into ASCII values
+    printf("String: %s\n", user_string);
+
+    while(user_string[array_count] != '\0')
+    {
+        char stringChar = user_string[array_count];
+        
+        //  REMOVE BEFORE ADDING TO MAIN--- TESTING 
+        printf("Char: %c\n", stringChar);
+
+        int asciiNum = stringChar;
+
+        //  REMOVE BEFORE ADDING TO MAIN--- TESTING 
+        printf("ASCII Value: %d\n", asciiNum);
+
+        ascii_array[array_count] = asciiNum;
+        array_count +=1;
+
+    }
+    
+    array_count = 0;
+
+    //  STEP 2: Convert each ascii value in array into binary number -- combine into one String
+    while(ascii_array[array_count] != '\0')
+    {
+        char *binaryNum = convertToBinary(ascii_array[array_count]);
+
+        //  REMOVE BEFORE ADDING TO MAIN--- TESTING 
+        printf("\nASCII: %d\nBINARY NUM: %s\n", ascii_array[array_count], binaryNum);
+
+        //  Concatenate the binary number to the binary string
+        strcat(binaryString, binaryNum);
+
+        //  REMOVE BEFORE ADDING TO MAIN--- TESTING
+        printf("\n Complete string: %s\n", binaryString);
+
+        //  Increase the index to change every ASCII number
+        array_count+=1;
+    }
+
+    array_count = 0;
+
+    //  STEP 3 and 4: Group array into groups of 6 equal bits and convert to DECIMAL
+    strcpy(spaced_array, split_array(binaryString, spaced_array));
+    char binary_number [100];
+
+    int number_index = 0;
+    int base_index = 0;
+
+    int decimal_numbers [100];
+
+    //  Get each char until whitespace and make it into a string
+
+    for(int i = 0; i < strlen(spaced_array); i++)
+    {
+        if(spaced_array[i] == ' ' || spaced_array[i] == '\0')
+        {
+            binary_number[number_index] = '\0';
+
+            //  REMOVE BEFORE ADDING TO MAIN--- TESTING
+            printf("6-bit Binary: %s\n", binary_number);
+
+            number_index = 0;
+
+            int dec_num = convertToDecimal(binary_number);
+
+            //  REMOVE BEFORE ADDING TO MAIN--- TESTING
+            printf("Decimal Value: %d\n", dec_num);
+
+            //  Convert decimal number into base64 alphabet
+            char base64_conversion = convertToBase(dec_num);
+
+            //  Add the char into string
+            base64_string[base_index] = base64_conversion;
+            base_index++;
+
+        }
+        else
+        {
+            binary_number[number_index] = spaced_array[i];
+            number_index++;
+
+        }
+    }
+
+    //  Check if string is multiple of 4 -- if not, add the necessary amount of 0's
+    if((strlen(base64_string) % 3) != 0)
+    {
+        //  Take string length to find the number of 0's needed
+        int padding_num = (4 - strlen(base64_string) % 4) % 4;
+        int padding_counter = 0;
+
+        while(padding_num > 0)
+        {
+            base64_string[strlen(base64_string) + padding_counter] = '=';
+            padding_num--;
+        }
+
+        //  Add null terminator
+        base64_string[strlen(base64_string) + padding_counter] = '\0';
+
+    }
+
+    printf("ENCRYPTED STRING: %s", base64_string);
 
 }
 
@@ -196,6 +321,98 @@ void Atbash_encryption() {
 
     printf("Result: %s\n", cipher);
 }
+
+
+// BASE64 FUNCTIONS
+char convertToBase(int decimalNum)
+{
+    char base64[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    return base64[decimalNum];
+
+}
+
+char* convertToBinary(int decimalNum)
+{
+    //  Allocate 9 chars: 8 bits + null terminator
+    char* bin = (char*) malloc(9 * sizeof(char));
+
+    //  Fill with '0's for 8 bits
+    for (int i = 0; i < 8; i++)
+    {
+        bin[7 - i] = (decimalNum % 2) + '0';
+        decimalNum /= 2;
+    }
+
+    //  Null terminator
+    bin[8] = '\0';
+
+    return bin;
+
+}
+
+int convertToDecimal(char* binaryNumber)
+{        
+    int decNum = 0;
+    int power = 1;
+
+    // Find the value of the binary number. With each iteration, increase the power by 2. 
+    for(int i = strlen(binaryNumber) - 1; i >= 0; i--)
+    {
+        if(binaryNumber[i] == '1')
+        {
+            decNum += power;
+        }
+        power = power * 2;
+
+    }
+    return decNum;
+
+}
+
+char* split_array(char input_array [1000], char output_array [2000])
+{
+    int counter = 0;
+    int inputArray_index = 0;
+    int outputArray_index = 0;
+
+    int remainder = strlen(input_array) % 6;
+    int num_zeroes = 6 - remainder;
+
+    // CHECK FOR 0 -- concatenate 0's to the end to complete the string
+    if(remainder != 0)
+    {
+        //  Add 0's and remove 0's needed
+        while(num_zeroes > 0)
+        {
+            strcat(input_array, "0");
+            num_zeroes--;
+        }
+    }
+
+    //  Split the strings with white space
+    while(input_array[inputArray_index] != '\0')
+    {
+        //  Add elements to output_array and increase counter
+        output_array[outputArray_index++] = input_array[inputArray_index++];
+        counter++;
+
+        //  Check when counter = 6, then add a space to the next space in the output_array
+        //  Reset counter to 0
+        if((counter == 6))
+        {
+            output_array[outputArray_index++] = ' ';
+            counter = 0;
+        }
+    }
+
+    //  Add the null terminator at the end of the output array to show end of string
+    output_array[outputArray_index] = '\0';
+
+    return output_array;
+
+}
+
 
 
 
